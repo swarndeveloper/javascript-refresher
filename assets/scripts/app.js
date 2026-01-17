1b618a57a6ce221464a4347b282f871f5e82fa2c
swarndeveloper <74850094+swarndeveloper@users.noreply.github.com>
2026-01-17 18:11:34 +0000

    Update on 17-01-2026

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,3 +4,48 @@ import { applicationName, version } from "./utils.js";
 console.log(`Application: ${applicationName}, Version: ${version}`);
 import * as Utils from "./utils.js";
 console.log("Current Date:", Utils.getCurrentDate());
+
+const myDetails = {
+  name: "Swarnendu",
+  age: "36",
+  printMyDetails() {
+    console.log("Myself " + this.name + ". My age is " + this.age);
+  },
+};
+
+console.log(myDetails.name);
+myDetails.printMyDetails();
+
+class Human {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  printHumanDetails() {
+    console.log("Hello, I am " + this.name + ". My age is ->" + this.age);
+  }
+}
+
+const man = new Human("Sohan", "8");
+console.log(man.name);
+man.printHumanDetails();
+
+const myFamily = ["Swarnendu", "Anasua", "Sohan"];
+
+const myPostion = myFamily.findIndex((item) => {
+  console.log(item);
+  item === "Sohan";
+});
+console.log(myPostion);
+
+const myNameDetailsFamily = myFamily.map((item) => ({
+  nameDetails: item,
+}));
+console.log(myNameDetailsFamily);
+
+const myFullNameDetailsFamily = myFamily.map((item) =>
+  item === "Anasua"
+    ? { firstName: item, lastname: "Chatterjee" }
+    : { firstName: item, lastname: "Ghosh" }
+);
+console.log(myFullNameDetailsFamily);
--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -4,3 +4,17 @@ export const version = "1.0.0";
 export function getCurrentDate() {
   return new Date().toISOString().split("T")[0];
 }
+
+const myName = (username) => {
+  return "My Name is" + username;
+};
+
+const myDetails = {
+  name: "Swarnendu",
+  age: "36",
+  printMyDetails() {
+    console.log("Myself " + this.name + ". My age is " + this.age);
+  },
+};
+
+myDetails.printMyDetails;
